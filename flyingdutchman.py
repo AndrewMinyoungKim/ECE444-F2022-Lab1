5fc332b86e7269759d88c7b597e2b12094ef527c
Andrew Kim <akim48415@gmail.com>
2022-09-17 17:32:18 -0400

    c4

---
+++ flyingdutchman.py
@@ -0,0 +1,1 @@
+print("Davy Jones' sock")
--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,7 @@
 def main():
     print('Hello World, I have been at UofT for 4 years now, since 2018')
     print("Jack Sparrow")
+    print("Spongebob")
 
 
 if __name__ == "__main__":
